cff174a32e00cabb3105a8807f117aec79a6e0d6
Sarthak Aggarwal <sarthakagg567@gmail.com>
2023-08-26 16:09:32 +0530

    Notebook

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
 from src.logger import logging
+import os
+from datetime import datetime
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -18,3 +20,10 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
